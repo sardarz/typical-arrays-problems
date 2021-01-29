ec19ddf6ac708b8679e27b69136f060c65509235
sardarz <muhkween@gmail.com>
2021-01-29 21:43:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || !array.length) return 0;
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || !array.length) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || !array.length) return 0;
+    return array.reduce((acc, cv) => acc + cv) / array.length
 }
